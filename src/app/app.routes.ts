0631f4cbc84aa2972508dffcd0e9f813758a85b8
ManuelGTM22 <eltitomusic.net@gmail.com>
2023-04-15 23:01:14 -0400

    chatApp

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'chat',
+    loadComponent: () => import('./pages/chat/chat.page').then( m => m.ChatPage)
+  },
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAx0UoPStNmaZ3oty0u1vV1OkyEtZukLoM",
+    authDomain: "chat-app-e4ae3.firebaseapp.com",
+    projectId: "chat-app-e4ae3",
+    storageBucket: "chat-app-e4ae3.appspot.com",
+    messagingSenderId: "93134800975",
+    appId: "1:93134800975:web:df6dfad4d835604c66823a",
+    measurementId: "G-EJK5FMV9F0"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
